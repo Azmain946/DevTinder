5175ee83c6f910587f9fbe26c2f5d8ab9b36a451
kijani23 <amnfq.abc@gmail.com>
2025-10-21 11:43:40 +0600

    get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.get("/user", (req, res)=>{
+    res.send({ firstName: "Azmain", lastName: "Fieque"});
+});
+
+app.post("/user", async (req, res)=>{
+    res.send("Data Successfully saved to the database");
+});
+
+app.delete("/user", (req, res)=>{
+    res.send("Deleted Successfully");
+});
+
 
 app.use("/test",(req, res)=>{
     res.send("Hello from the test!");
 });
-app.use("/",(req, res)=>{
-    res.send("Hello from the server!");
-});
+
 app.listen(3000, ()=>{
     console.log("Server is successfully listening on port 3000...");
 })
