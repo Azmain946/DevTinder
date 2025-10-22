c633b63e448b1c2a31bda1f4c8dcc35ce83bcdec
kijani23 <amnfq.abc@gmail.com>
2025-10-22 12:54:28 +0600

    auth, middleware

--- src/app.js
+++ src/app.js
@@ -1,22 +1,16 @@
 const express = require("express");
+const { adminAuth } = require("./middlewares/auth");
 const app = express();
 
-app.get("/user", (req, res)=>{
-    res.send({ firstName: "Azmain", lastName: "Fieque"});
-});
+app.use("/admin",adminAuth)
 
-app.post("/user", async (req, res)=>{
-    res.send("Data Successfully saved to the database");
-});
+app.get("/admin/getAllData", (req, res)=>{
+    res.send("All Data Sent");
+})
 
-app.delete("/user", (req, res)=>{
-    res.send("Deleted Successfully");
-});
-
-
-app.use("/test",(req, res)=>{
-    res.send("Hello from the test!");
-});
+app.get("/admin/deleteUser", (req, res)=>{
+    res.send("Deleted a user");
+})
 
 app.listen(3000, ()=>{
     console.log("Server is successfully listening on port 3000...");
