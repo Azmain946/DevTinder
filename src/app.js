1921c0f09b25cbf7bc0cdcbac584c3a61da5723c
kijani23 <amnfq.abc@gmail.com>
2025-10-25 09:22:23 +0600

    get apis

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const User = require("./models/user");
 app.use(express.json());
 
 app.post("/signup", async(req, res)=>{
+    console.log(req.body);
     const user = new User(req.body);
 
     try {
@@ -17,6 +18,33 @@ app.post("/signup", async(req, res)=>{
     }
     
 })
+
+app.get("/user", async (req, res)=>{
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.find({emailId: userEmail});
+        if (users.length===0) {
+            res.status(404).send("User not found");
+
+        } else {
+        res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+app.get("/feed", async (req, res)=>{
+    
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+
 
 connectDB()
     .then(()=>{
