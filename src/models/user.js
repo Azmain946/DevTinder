c6e9f41f97079de9b364c0cc4f9d142efbcf45d7
kijani23 <amnfq.abc@gmail.com>
2025-10-25 20:05:48 +0600

    schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,48 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String
+  firstName: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: 18,
+  },
+  gender: {
+    type: String,
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
+        throw new Error("Gender Data is not correct!");
+      }
     },
-    lastName: {
-        type: String
-    }, 
-    emailId: {
-        type: String
-    },
-    password: {
-        type: String
-    },
-    age: {
-        type: Number
-    },
-    gender: {
-        type: String
-    }
-});
+  },
+  photoUrl: {
+    type: String,
+  },
+  about: {
+    type: String,
+    default: "This is default about the user!",
+  },
+  skills: {
+    type: [String],
+  },
+}, {timestamps: true}); 
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
